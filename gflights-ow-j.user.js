30fdd6f471427d7aeb4aa7f6d5f2eb4a6a9d4e17
mbesemann <42157326+mbesemann@users.noreply.github.com>
2023-09-17 20:16:54 -0400

    Update gflights-ow-j.user.js
    Add default currency & country support

--- gflights-ow-j.user.js
+++ gflights-ow-j.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Flights OW J
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Sets Google Flights params to One-Way & Business class.
 // @author       PimpChicken
 // @match        https://www.google.com/travel/flights*
@@ -13,7 +13,17 @@
 (function() {
     'use strict';
 
+if ('URLSearchParams' in window) {
+    var searchParams = new URLSearchParams(window.location.search);
+    if(searchParams.get('curr') != 'USD' || searchParams.get('gl') != 'US') {
+        searchParams.set('curr', 'USD'); // Replace USD with preferred currency
+        searchParams.set('gl', 'US'); // Repalce US with preferred country
+        window.location.search = searchParams.toString();
+    }
+}
+
 document.querySelectorAll('.TQYpgc.gInvKb ul')[0].children[2].click();
 document.querySelectorAll('.JQrP8b.PLrkBc ul')[0].children[3].click();
+
 
 })();
